a7007382e5d5ff36f13535a4889723208de2192d
Mohammad Irfanul Islam <irfanul009@gmail.com>
2026-07-29 20:55:08 +0600

    Contact Me JS Added

--- scripts/script.js
+++ scripts/script.js
@@ -22,3 +22,17 @@ function setActiveLink() {
 }
 setActiveLink();
 window.addEventListener("hashchange", setActiveLink);
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxeYyAT4YofNFX4K6z9M2MkUCZ_yHIvHGTMD83yYOu0rAcTMtxaz0IT7WVYzsuuinGlbw/exec'
+const form = document.getElementById('contactForm')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            form.reset();
+            console.log('Success!', response)
+        })
+        .catch(error => console.error('Error!', error.message))
+})
